d28e7107f9b6b37925755b79158e32ebe28beb44
nguynentrungmen <nguyentrungmen@gmail.com>
2024-05-26 20:44:12 +0700

    committed

--- commands.sh
+++ commands.sh
@@ -1,9 +1,2 @@
-# Run terraform
-cd terraform
-terraform init
-terraform plan -out solution.plan
-terraform apply "solution.plan"
-
-cd ..
 # DEPLOY APP
 az webapp up --name mennt1-udacity-project2-app --resource-group Azuredevops --sku B1 --logs --runtime "PYTHON:3.9" --location "southeastasia"
