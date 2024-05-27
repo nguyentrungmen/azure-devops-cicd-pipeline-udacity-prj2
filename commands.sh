e257ecc7e1a58bcd7f73ca601a23afdc150cd242
nguynentrungmen <nguyentrungmen@gmail.com>
2024-05-27 20:13:15 +0700

    commited

--- commands.sh
+++ commands.sh
@@ -1,2 +1,12 @@
+
+# Clone source 
+git clone git@github.com:nguyentrungmen/azure-devops-cicd-pipeline-udacity-prj2.git; 
+cd azure-devops-cicd-pipeline-udacity-prj2; 
+git pull; 
+python3 -m venv ~/.azure-devops-cicd-pipeline-udacity-prj2; 
+# Active ENV
+source ~/.azure-devops-cicd-pipeline-udacity-prj2/bin/activate; 
+# Install requirements
+make all; 
 # DEPLOY APP
-az webapp up --name mennt1-udacity-project2-app --resource-group Azuredevops --sku B1 --logs --runtime "PYTHON:3.9" --location "southeastasia"
+az webapp up -n mennt1-udacity-project2-app -g Azuredevops --sku FREE
